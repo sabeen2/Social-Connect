65cf3711ffeeaaa4b0533f4e1f689b9e7b0f0658
Sabin Pandey <sabeenpandey2@gmail.com>
2024-07-27 23:26:55 +0545

    feed and post comp

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="p-4 bg-white rounded-lg justify-between text-sm flex flex-col gap-12">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 };
 
 export default Feed;
